ac11d56f3829ea4405aeee9fbef2355940371264
Rai925 <154880397+Rai925@users.noreply.github.com>
2024-04-14 22:12:25 +0300

    Update task-1.py

--- task-1.py
+++ task-1.py
@@ -137,7 +137,7 @@ class AddressBook(UserDict):
 
     def find(self, name: str) -> Record:
         if name in self.data:
-            return self.data[name]
+            return self.data.get(name)
         else:
             return None
         
